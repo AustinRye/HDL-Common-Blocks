e99051cf2c95d11212d00e431c15fdab344e09fb
AustinWRye <RyeAustinW@gmail.com>
2020-06-19 23:46:03 -0400

    FIFO -- Add fifo logic

--- fifo/rtl/fifo.sv
+++ fifo/rtl/fifo.sv
@@ -21,6 +21,37 @@ module fifo
         output logic full   // full flag
     );
 
+    logic [DATA_WIDTH] mem [2**ADDR_WIDTH-1:0]; // memory
+    logic [ADDR_WIDTH-1:0] wr_ptr; // write pointer
+    logic [ADDR_WIDTH-1:0] rd_ptr; // read pointer
     
+    always_ff @(posedge clk)
+        if (!rstn)
+        begin
+            data_out <= 0;
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            empty <= 1;
+            full <= 0;
+        end
+        else
+        begin
+            if (wr_en & !full)
+            begin
+                mem[wr_ptr] <= data_in;
+                empty <= 0;
+                wr_ptr <= wr_ptr + 1;
+                if (wr_ptr == rd_ptr)
+                    full <= 1;
+            end
+            if (rd_en & !empty)
+            begin
+                data_out <= mem[rd_ptr];
+                full <= 0;
+                rd_ptr <= rd_ptr + 1;
+                if (wr_ptr == rd_ptr)
+                    empty <= 1;
+            end
+        end
 
 endmodule
