36da10229ec70ac44e7abc556ca62202a0d722fc
AustinWRye <RyeAustinW@gmail.com>
2020-08-14 22:04:15 -0400

    FIFO -- Change addr parameter to depth

--- fifo/rtl/fifo.sv
+++ fifo/rtl/fifo.sv
@@ -8,31 +8,31 @@
 
 module fifo
     #(
-        parameter DATA_WIDTH = 8,
-        parameter ADDR_WIDTH = 8
+        parameter WIDTH = 8,
+        parameter DEPTH = 8
     ) (
         input  logic clk,   // clock
         input  logic rstn,  // reset active low
         input  logic wr_en, // write enable
         input  logic rd_en, // read enable
-        input  logic [DATA_WIDTH-1:0] data_in,  // data in
-        output logic [DATA_WIDTH-1:0] data_out, // data out
+        input  logic [WIDTH-1:0] data_in,  // data in
+        output logic [WIDTH-1:0] data_out, // data out
         output logic empty, // empty flag
         output logic full   // full flag
     );
 
-    logic [DATA_WIDTH] mem [2**ADDR_WIDTH-1:0]; // memory
-    logic [ADDR_WIDTH-1:0] wr_ptr; // write pointer
-    logic [ADDR_WIDTH-1:0] rd_ptr; // read pointer
+    logic [WIDTH-1] mem [0:DEPTH];  // memory
+    logic [$clog2(DEPTH):0] wr_ptr; // write pointer
+    logic [$clog2(DEPTH):0] rd_ptr; // read pointer
     
     always_ff @(posedge clk)
         if (!rstn)
         begin
             data_out <= 0;
-            wr_ptr <= 0;
-            rd_ptr <= 0;
-            empty <= 1;
-            full <= 0;
+            wr_ptr   <= 0;
+            rd_ptr   <= 0;
+            empty    <= 1;
+            full     <= 0;
         end
         else
         begin
@@ -40,7 +40,7 @@ module fifo
             begin
                 mem[wr_ptr] <= data_in;
                 empty <= 0;
-                wr_ptr <= wr_ptr + 1;
+                wr_ptr <= (wr_ptr + 1) % DEPTH;
                 if (wr_ptr == rd_ptr)
                     full <= 1;
             end
@@ -48,7 +48,7 @@ module fifo
             begin
                 data_out <= mem[rd_ptr];
                 full <= 0;
-                rd_ptr <= rd_ptr + 1;
+                rd_ptr <= (rd_ptr + 1) % DEPTH;
                 if (wr_ptr == rd_ptr)
                     empty <= 1;
             end
